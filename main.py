92e7ce0a0fc570d8cc70383fafee2de7f05bb5bd
dmitry <dmitrymatio@gmail.com>
2020-02-27 22:42:39 -0800

    reminder initial logic in oop format

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-menu = """
-Reminders menu:
-
-1. show all reminders
-2. search reminders
-3. add reminders
-4. modify reminders
-5. export reminders
-6. import reminders
-7. quit
-"""
-
 class Reminder():
+
     def __init__(self, id, tags, text):
         self.id = id
         self.text = text
         self.tags = tags
-    
+
     def getRem(self):
         return f"""
         Reminder {self.id}, 
@@ -23,32 +12,58 @@ class Reminder():
                         '{self.text}''   
         """
 
-class ReminderList():
+
+class ListOfReminders():
+
     def __init__(self):
         self.reminders = []
-    def addReminder(self,reminder):
-        self.reminders.insert(0,reminder)
+
+    def addReminder(self, reminder):
+        self.reminders.insert(0, reminder)
         print(f"Reminder added. id # = {reminder.id}")
+
     def showReminders(self):
         for reminder in self.reminders:
             print(reminder.getRem())
+
     def modReminder(self, id, tags, text):
         select = self.reminders[-id].getRem()
         print(select)
 
+    def exportRems(self):
+        pass
 
-reminder1 = Reminder(1,911,"Hello world")
-reminder2 = Reminder(2,911,"Hello world")
-reminder3 = Reminder(3,911,"Hello world")
-
-list = ReminderList()
-
-list.addReminder(reminder1)
-list.addReminder(reminder2)
-list.addReminder(reminder3)
-list.showReminders()
-list.modReminder(1,1,1)
-list.modReminder(2,1,1)
-list.modReminder(3,1,1)
+    def importRems(self):
+        pass
 
 
+class remindersComp():
+    menu = """
+    Reminders menu:
+
+    1. show all reminders
+    2. search reminders
+    3. add reminders
+    4. modify reminders
+    5. export reminders
+    6. import reminders
+    7. quit
+    """
+
+    @classmethod
+    def runComp(cls):
+        remList = ListOfReminders()
+        
+        exit = False
+        while not exit:
+            userInput = int(input(cls.menu))
+            print(userInput)
+            if (userInput == 3):
+                rem1 = Reminder(1, ['911','family'], "hello world 1")
+                remList.addReminder(rem1)
+                remList.showReminders()
+            exit = True
+
+
+
+remindersComp.runComp()
